edfb159897bbbf7e8a7a82c0af3083c2bea07a64
xdawoodx2 <dawood.waheed@itcs.com.pk>
2025-11-18 10:39:53 +0500

    Create insert.php

---
+++ insert.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "test-srvr01.database.windows.net";
+$username   = "root";
+$password   = "your_password";
+$dbname     = "your_database";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$name  = $_POST['name'];
+$email = $_POST['email'];
+
+$sql = "INSERT INTO test_table (name, email) VALUES ('$name', '$email')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Record inserted successfully!<br>";
+    echo "<a href='form.html'>Back to form</a>";
+} else {
+    echo "Error: " . $conn->error;
+}
+
+$conn->close();
+?>
