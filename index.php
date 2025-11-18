4f998dbb9b0f3b88ea1c7e4c51efbfd6e689b0fb
xdawoodx2 <dawood.waheed@itcs.com.pk>
2025-11-18 10:29:59 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,9 @@
-    <?php
-    $servername = "test-srvr01"; // Or your database server IP/hostname
-    $username = "Dawood";
-    $password = "123@Itcs";
-    $dbname = "db01";
-
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    echo "Connected successfully to MySQL database!";
-
-    // Close connection
-    $conn->close();
-    ?>
 <?php
 
 echo "Hello World";
 
 ?>
+
+
+
+
